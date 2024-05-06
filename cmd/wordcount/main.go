a900b8fc03ad68181916847b03fba9fe545fb8a3
Hongyi Li <h2323547425@gmail.com>
2024-05-06 09:42:44 -0400

    update mp

--- cmd/wordcount/main.go
+++ cmd/wordcount/main.go
@@ -3,39 +3,46 @@ package main
 import (
 	"fmt"
 	"runtime"
-	// "os"
-	// "strconv"
-	// "strings"
+	"os"
+	"strconv"
+	"strings"
 	"word-count/pkg/fileparser"
 	"word-count/pkg/mapreduce"
 )
 
 func main() {
-	// if len(os.Args) != 4 {
-	// 	fmt.Println("[Usage] wordcount <filename> <num splits> <query word>")
-	// 	return
-	// }
+	runtime.GOMAXPROCS(8)
 
-	// filename := os.Args[1]
-	// nSplit, err := strconv.Atoi(os.Args[2])
-	// if err != nil {
-	// 	fmt.Println("Invalid number of splits")
-	// 	return
-	// }
-	// word := strings.ToLower(os.Args[3])
+	if len(os.Args) < 4 || len(os.Args) > 5 {
+		fmt.Println("[Usage] wordcount <filename> <num splits> <query word> <optional: num repeats>")
+		return
+	}
 
-	// inputs, err := fileparser.SplitFile(filename, nSplit)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
+	filename := os.Args[1]
+	nSplit, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Println("Invalid number of splits")
+		return
+	}
+	word := strings.ToLower(os.Args[3])
+	var nRepeat int
+	if len(os.Args) == 5 {
+		nRepeat, err = strconv.Atoi(os.Args[4])
+		if err != nil {
+			fmt.Println("Invalid number of repeats")
+			return
+		}
+	}
 
-	runtime.GOMAXPROCS(2)
-
-	inputs := make([][]string, 0)
-	inputs = append(inputs, fileparser.ParseString("Hello World Hello"))
-	inputs = append(inputs, fileparser.ParseString("World World Hello"))
-	word := "world"
+	inputs, err := fileparser.SplitFile(filename, nSplit)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// inputs := make([][]string, 0)
+	// inputs = append(inputs, fileparser.ParseString("Hello World Hello"))
+	// inputs = append(inputs, fileparser.ParseString("World World Hello"))
+	// word := "world"
 
 	mapper := func(input []string) map[string]int {
 		result := make(map[string]int)
@@ -50,6 +57,10 @@ func main() {
 			result += value
 		}
 		return result
+	}
+
+	for i := 0; i < nRepeat - 1; i++ {
+		mapreduce.MapReduce[[]string, string, int, int](inputs, mapper, reducer)
 	}
 	result := mapreduce.MapReduce[[]string, string, int, int](inputs, mapper, reducer)
 	fmt.Println(result[word])
